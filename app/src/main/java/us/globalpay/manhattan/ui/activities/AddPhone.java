a1db60e96f2a4853004ec9c74fdd32f71e37597b
Josué Chávez <jchavez@globalpay.us>
2018-09-13 09:26:24 -0600

    # Se creó layout de intro de telefono # Se importaron recursos gráficos

---
+++ app/src/main/java/us/globalpay/manhattan/ui/activities/AddPhone.java
@@ -0,0 +1,17 @@
+package us.globalpay.manhattan.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import us.globalpay.manhattan.R;
+
+public class AddPhone extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_phone);
+    }
+}
